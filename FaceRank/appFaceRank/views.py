0900c87ef44f5cb63b61d64a75b480010d6e6e8a
mc <miniron0823@naver.com>
2021-01-31 18:10:07 +0900

    coffebene day

--- FaceRank/appFaceRank/views.py
+++ FaceRank/appFaceRank/views.py
@@ -4,11 +4,12 @@ import simplejson as json
 from appFaceRank import views
 from django.shortcuts import render
 import tensorflow.keras
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFont, ImageDraw
 import numpy as np
 import cv2
-import base64, io
-#from datauri import DataURI
+import base64
+import io
+# from datauri import DataURI
 
 
 def index(request):
@@ -22,14 +23,16 @@ def getPicture(request):
 
     imageUrl = request.POST['search_key']
     im = Image.open(io.BytesIO(base64.b64decode(str(imageUrl).split(',')[1])))
-    #im.save("ttttttsssss.jpg") 
+    # im.save("ttttttsssss.jpg")
     context = {}
-    
-    #image = Image.open(context)
-    
-    #sample_image = cv2.imread(context)
 
-    #getImage(im)
+    # image = Image.open(context)
+
+    # sample_image = cv2.imread(context)
+
+    sample_image  = getImage(im)
+    context['src'] = sample_image
+
     return HttpResponse(json.dumps(context), content_type="application/json")
 
 
@@ -37,42 +40,57 @@ def getImage(imgName):
     img = []
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    sample_image = np.array(imgName)#cv2.imread('btsjpg.jpg')
-    
+    sample_image = np.array(imgName)  # cv2.imread('btsjpg.jpg')
+
     gray = cv2.cvtColor(sample_image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     bigValue = -9999999
     bigidx = 0
 
-    idx = 0
+    idx = 1
+    rank = []
     for (x, y, w, h) in faces:
 
         cv2.rectangle(sample_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
         sub_face = sample_image[y:y+h, x:x+w]
         PIL_image = Image.fromarray(np.uint8(sub_face)).convert('RGB')
-        #test = Image.open(PIL_image)
+        # test = Image.open(PIL_image)
         # test.show()
         result = exeTeachableMachine(PIL_image)
 
-        if bigValue < result[0][1]:
-            bigValue = result[0][1]
+        # print(str(idx) + ',' + str(result[0][1]))
+        # if bigValue < result[0][1]:
+        #    bigValue = result[0][1]
+        #    rank = idx
         idx = idx+1
-        #FaceFileName = "unknowfaces/face_" + str(y) + ".jpg"
-        #cv2.imwrite(FaceFileName, sub_face)
-    rank = idx + 1
-    #print('1등은 몇번째 사진인가효???'+rank)
-    #cv2.imshow('sample', sample_image)
-    # cv2.waitKey()
+        # FaceFileName = "unknowfaces/face_" + str(y) + ".jpg"
+        # cv2.imwrite(FaceFileName, sub_face)'
+        label = str(idx)
+        Y = y - 10 if y - 10 > 10 else y + 10
+
+    rank.sort(reverse=True)
+    for i, val in enumerate(rank):
+        cv2.putText(sample_image, str(i+1), (rank[i][2], rank[i][3]),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    #cv2.imshow('face-image', sample_image)
+    #cv2.waitKey()
+    # print('1등은 몇번째 사진인가효???'+str(rank))
+
     # print(img)
     # return img
-
+    rgbImage = cv2.cvtColor(sample_image, cv2.COLOR_BGR2RGB)
+    retval , image_buffer = cv2.imencode('.jpg', rgbImage)
+    jpg_as_text = base64.b64encode(image_buffer)
+    return jpg_as_text
 
 def exeTeachableMachine(image):
     # Disable scientific notation for clarity
     np.set_printoptions(suppress=True)
 
     # Load the model
-    model = tensorflow.keras.models.load_model('keras_model.h5')
+    model = tensorflow.keras.models.load_model('keras_model.h5', compile=False)
 
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
@@ -81,7 +99,7 @@ def exeTeachableMachine(image):
 
     # getImage()
     # Replace this with the path to your image
-    #image = Image.open('sample.jpg')
+    # image = Image.open('sample.jpg')
 
     # resize the image to a 224x224 with the same strategy as in TM2:
     # resizing the image to be at least 224x224 and then cropping from the center
@@ -92,7 +110,7 @@ def exeTeachableMachine(image):
     image_array = np.asarray(image)
 
     # display the resized image
-    image.show()
+    # image.show()
 
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
@@ -107,5 +125,5 @@ def exeTeachableMachine(image):
 
 if __name__ == '__main__':
     getImage('btsjpg.jpg')
-    #exeTeachableMachine('sample.jpg')
+    # exeTeachableMachine('sample.jpg')
     # exeTeachableMachine(getImage('btsjpg.jpg'))
